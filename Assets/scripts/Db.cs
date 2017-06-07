f7fcf360e598d0c44df95ad6ae0d2c1d05eb1e5d
Ben-PC\Ben <tylerkillspoon@gmail.com>
2017-06-07 15:47:34 -0400

    Added Db class and JSONObject plugin to retrieve and parse Db rows.

---
+++ Assets/scripts/Db.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Db : MonoBehaviour {
+
+    public string profile_url = "https://mage-match.herokuapp.com/api/message";
+
+    private JSONObject profileJson;
+
+    // Use this for initialization
+    void Start () {
+        StartCoroutine(GetData());
+	}
+
+    public IEnumerator GetData() {
+        yield return RetrieveProfileInfo();
+
+        JSONObject j = new JSONObject(profileJson);
+        //accessData(j);
+        Debug.Log(j.Print());
+
+        JSONObject prefs = j.list[0].list[0].list[3];
+        Debug.Log(prefs.Print());
+
+        for (int i = 0; i < prefs.list.Count; i++) {
+            JSONObject obj = prefs.list[i];
+            Debug.Log("\"" + prefs.keys[i] + "\": " + obj.n);
+        }
+    }
+
+    public IEnumerator RetrieveProfileInfo() {
+        WWW www = new WWW(profile_url);
+        yield return www;
+        profileJson = new JSONObject(www.text);
+    }
+
+    public JSONObject GetProfilePrefs(int i) {
+        if (i < profileJson.list[0].list.Count)
+            return profileJson.list[0].list[i].list[3]; //data.user.prefs object
+        else {
+            Debug.LogError("There's no player with that id!!");
+            return null;
+        }
+    }
+
+    void accessData(JSONObject obj) {
+        switch (obj.type) {
+            case JSONObject.Type.OBJECT:
+                Debug.Log("{");
+                for (int i = 0; i < obj.list.Count; i++) {
+                    string key = (string)obj.keys[i];
+                    JSONObject j = (JSONObject)obj.list[i];
+                    Debug.Log("\"" + key + "\":");
+                    accessData(j);
+                }
+                Debug.Log("}");
+
+                break;
+            case JSONObject.Type.ARRAY:
+                Debug.Log("[");
+                foreach (JSONObject j in obj.list) {
+                    accessData(j);
+                    Debug.Log(", ");
+                }
+                Debug.Log("]");
+                break;
+            case JSONObject.Type.STRING:
+                Debug.Log(obj.str);
+                break;
+            case JSONObject.Type.NUMBER:
+                Debug.Log(obj.n);
+                break;
+            case JSONObject.Type.BOOL:
+                Debug.Log(obj.b);
+                break;
+            case JSONObject.Type.NULL:
+                Debug.Log("NULL");
+                break;
+
+        }
+    }
+}
---
+++ Assets/scripts/PlayerProfile.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerProfile : MonoBehaviour {
+
+    public Sprite[] frames, backgrounds, foregrounds;
+
+	// Use this for initialization
+	void Start () {
+        Db db = GameObject.Find("test").GetComponent<Db>();
+        db.
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public IEnumerator ShowProfiles() {
+        
+    }
+
+}
